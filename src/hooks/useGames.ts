e78357ddb10ff87412fc6d8bd99fe27c05e6b539
Daniel Mariano <93936803+zePiCzz@users.noreply.github.com>
2024-11-15 20:03:31 +0000

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,5 @@
 import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 
 export interface Platform{
     id: number;
@@ -20,7 +19,8 @@ export interface Game {
 const useGames = (gameQuery: GameQuery) => 
   useData<Game>("/games", {params: {
     genres: gameQuery.genre?.id, 
-    platforms: gameQuery.platform?.id
+    platforms: gameQuery.platform?.id,
+    ordering: gameQuery.sortOrder
   }}, 
   [gameQuery]);  
 
